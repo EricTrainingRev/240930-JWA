94e3a08b8a84bb2b330c4eb9a8061f6436f93f5c
EricTrainingRev <eric.suminski@revature.com>
2024-10-14 09:54:38 -0400

    adding business rule implementation to createMember service method

--- Week-2/LibraryApp/src/main/java/com/revature/LibraryApp/services/MemberService.java
+++ Week-2/LibraryApp/src/main/java/com/revature/LibraryApp/services/MemberService.java
@@ -20,8 +20,30 @@ public class MemberService {
     }
 
     public Member createMember(Member member) {
-        // TODO: IMPLEMENT VALIDATION to make sure the member is valid information associated with the business rules
-        return memberDao.create(member);
+        /*
+        TODO: IMPLEMENT VALIDATION to make sure the member is valid information associated with the business rules
+        Business rule: to make passwords more secure they must have an upper and lowercase letter
+        */ 
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        for (char character : member.getPassword().toCharArray()){
+            if(Character.isLowerCase(character)){
+                hasLower = true;
+                break;
+            }
+        }
+        for(char character2 : member.getPassword().toCharArray()){
+            if(Character.isUpperCase(character2)){
+                hasUpper = true;
+                break;
+            }
+        }
+        if(hasUpper && hasLower){
+            return memberDao.create(member);
+        } else {
+            return null;
+        }
+        
     }
 
     public Member updateMember(int memberId, Member member) {
